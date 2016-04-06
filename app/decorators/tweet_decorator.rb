b8a61ea369bfe40b38414b567ff40e65a86e0646
William Kina <thekina@gmail.com>
2016-04-06 03:40:14 -0300

    mention parser

--- app/decorators/tweet_decorator.rb
+++ app/decorators/tweet_decorator.rb
@@ -4,4 +4,13 @@ class TweetDecorator < Draper::Decorator
   def time
     created_at.strftime "%d/%m/%Y %H:%M"
   end
+
+
+  def text
+    object.text.gsub(MENTION_REGULAR_EXPRESSION, h.link_to('\0', "#{TWITTER_URL+'\1'}") )
+  end
+
+  private
+  MENTION_REGULAR_EXPRESSION = /@([a-zA-Z0-9_\-]+)/i #/\B\@([\w\-]+)/i
+  TWITTER_URL = 'http://www.twitter.com/'
 end
